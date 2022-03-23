a110a472becbefb48926344e96544c1a752ad249
Ulaş <arifulasdemirdoven2000@gmail.com>
2022-03-23 16:45:12 +0300

    Update ATMrush.cs

--- Assets/Scripts/ATMrush.cs
+++ Assets/Scripts/ATMrush.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ATMrush : MonoBehaviour
 {
@@ -22,6 +23,24 @@ public class ATMrush : MonoBehaviour
     
     public void StackCube(GameObject other , int index)
     {
-
+        other.transform.parent = transform;
+        Vector3 newPos = cubes[index].transform.localPosition;
+        newPos.z += 0.25f;
+        other.transform.localPosition = newPos;
+        StartCoroutine(MakeObjectsBigger());
     }
+
+    public IEnumerator MakeObjectsBigger()
+    {
+        for (int i = cubes.Count; i > 0; i--)
+        {
+            Vector3 scale = new Vector3(0.25f, 0.25f, 0.25f);
+            scale *= 1.5f;
+
+            cubes[i].transform.DOScale(scale, 0.1f).OnComplete(() =>
+            cubes[i].transform.DOScale(new Vector3(0.25f, 0.25f, 0.25f), 0.1f));
+            yield return new WaitForSeconds(0.05f);
+        }
+
+    } 
 }
