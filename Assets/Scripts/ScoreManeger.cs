ed242786d65cf49810ebb24ed589702d8f5d8a93
Ulaş <arifulasdemirdoven2000@gmail.com>
2022-04-10 19:00:55 +0300

    UpdateV20

--- Assets/Scripts/ScoreManeger.cs
+++ Assets/Scripts/ScoreManeger.cs
@@ -7,13 +7,20 @@ public class ScoreManeger : MonoBehaviour
 {
     private float coin = 0;
     public Text CoinText;
+    ParticleSystem takeParicle;
 
+
+    private void Start()
+    {     
+        takeParicle = GameObject.Find("take").GetComponent<ParticleSystem>();
+        takeParicle.Stop();
+    }
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.CompareTag("Coin"))
         {
-            
+            takeParicle.Play();
             coin++;
             CoinText.text = coin.ToString();
             Destroy(other.gameObject);
