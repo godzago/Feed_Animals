c410fde72fd9cda1e7589561eb908e8188f527e7
Ulaş <arifulasdemirdoven2000@gmail.com>
2022-04-13 18:31:02 +0300

    UpdateV23
    Restart Button

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
-public class RestartGame : MonoBehaviour
+
+public class RestartGame : MonoBehaviour  
 {
+    public GameObject RestartScreen;
     private void OnTriggerEnter(Collider other)
     {
+        Time.timeScale = 0;
+        RestartScreen.SetActive(true);
+    }
+    public void Restart()
+    {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1;
     }
 }
